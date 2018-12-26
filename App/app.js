6d7635a682c06d00e33f39bf9e09a86b2541d2a1
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2018-12-26 22:59:18 +0000

    testing sending notifications

--- App/app.js
+++ App/app.js
@@ -25,6 +25,42 @@ global.mtqqLocalPath = "mqtt://piscos.tk";
     ref.on("value", function(snapshot) {
       console.log(snapshot.val());
     });
+
+    var mqttCluster=await mqtt.getClusterAsync() 
+    mqttCluster.subscribeData("testValve/turn", function(content) {
+        
+
+        var topic = 'zonesalerts';
+
+        // See documentation on defining a message payload.
+        var message = {
+            data: {
+              score: '850',
+              time: '2:45'
+            },
+            notification: {
+                title: '$GOOG up 1.43% on the day',
+                body: content.toString()
+              },
+            topic: topic
+          };
+        
+        // Send a message to devices subscribed to the provided topic.
+        admin.messaging().send(message)
+          .then((response) => {
+            // Response is a message ID string.
+            console.log('Successfully sent message:', response);
+          })
+          .catch((error) => {
+            console.log('Error sending message:', error);
+          });
+
+
+
+
+    });
+
+
   })();
 
   
