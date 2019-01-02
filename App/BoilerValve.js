e5ce5c508109bb73b2a4ac297ae7c45b17bc4fd3
miguel <mig.ruiz@gmail.com>
2019-01-02 17:37:04 +0000

    main log

--- App/BoilerValve.js
+++ App/BoilerValve.js
@@ -4,6 +4,7 @@ const MINUTESTOMONITOR=60
 class BoilerValve{
     constructor(valveCode) {      
        this.valveCode=valveCode;
+       this.lastState;
        this.history={}
     }
     async initAsync(){
@@ -15,8 +16,12 @@ class BoilerValve{
         
         var now=Math.floor(Date.now() / 1000);
         var keys=Object.keys(this.history)
+        if (this.valveCode=="test"){
+
+        }
         var starTimeMonitorInterval=now - 60 * MINUTESTOMONITOR
         var keysToDelete=keys.filter(k=>parseInt(k)<=starTimeMonitorInterval)
+        keysToDelete.pop()
         for (let index = 0; index < keysToDelete.length; index++) {
             const key = keysToDelete[index];
             delete this.history[key]
@@ -28,22 +33,27 @@ class BoilerValve{
             lastTimeWasOn=starTimeMonitorInterval
         }
         for (var key in this.history) {
-            if (this.history[key]==false){
-                counter=counter+(key-lastTimeWasOn)
+            if (this.history[key]==false && lastTimeWasOn){
+                counterOnSecs=counterOnSecs+(parseInt(key)-lastTimeWasOn)
             }
             if (this.history[key]==true){
-                lastTimeWasOn=key;
+                lastTimeWasOn=parseInt(key);
             }
         }
         var lastKey=Object.keys(this.history).reverse()[0];
         if (this.history[lastKey]==true){
-            counter=counter+(now-lastTimeWasOn)
+            counterOnSecs=counterOnSecs+(now-lastTimeWasOn)
         }
+        if (counterOnSecs)
+        console.log(this.valveCode + counterOnSecs)
 
     }
     onZoneValveChanged(state){
-        var now=Math.floor(Date.now() / 1000);
-        this.history[now]=state
+        if (this.lastState!=state){
+            var now=Math.floor(Date.now() / 1000);
+            this.history[now]=state
+            this.lastState=state;
+        }
       }
 }
 
